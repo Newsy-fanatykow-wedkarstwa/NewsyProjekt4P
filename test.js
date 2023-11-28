437280a18ac821aae9d5a3040fd4e77304a89c8c
IdaKania <ida.kania@wp.pl>
2023-11-28 13:16:48 +0100

    Walidacja dla wpisywania ile newsow

--- test.js
+++ test.js
@@ -34,38 +34,38 @@ const showPosts = (start,howMany,url) => { //najnowsze posty
     fetch(url)
         .then(response => response.json())
         .then (async json => {
-            for(var a=start; a<howMany; a++) {
-                let x= json[a];
-                await fetch('https://hacker-news.firebaseio.com/v0/item/'+x+'.json?print=pretty')
-                    .then(response2 => response2.json())
-                    .then(json2 => {
-                        console.log(json2.time);
-                        let wynik = '<div class="post" id="div'+(a+1)+'"><p class="ID_posta">ID: <span id="wynik_ID">'+(a+1)+'</span>';
-                        wynik += '<p class="tytul_posta">';
-                        wynik += '<span id="tytul_posta">tytul: '+json2.title+'</span></p>';
-                        wynik += '<img class="triangle0" id="triangle'+(a+1)+'" src="obrazki/white_triangle.png" onclick="imageChange('+(a+1)+')" style="width: 15px;">&emsp;';
-                        wynik += '<p class="link_posta"><span id="link_posta">';
-                        if(typeof json2.url == 'undefined') {
-                        wynik += '<a href="#">brak</a>';
-                        }
-                        else {
-                            wynik += '(<a href="'+json2.url+'">link</a>)';
-                        }
-                        wynik += '</span></p><br>';
-                        wynik += '<button class="btn btn-secondary" id="hide" onclick="hide('+(a+1)+')">hide</button>&emsp;';
-                        wynik += '<p class="wynik_posta"><span id="wynik_posta">wynik: '+json2.score+'</span></p>';
-                        wynik += '<p class="autor_posta"><span id="autor_posta">autor: '+json2.by+'</span></p>';
-                        wynik += '<p class="data_posta"><span id="data_posta">'+timeAgo(Date.now()/1000-json2.time)+'</span></p>';
-                        wynik += '<p class="ilosc_komment"><span id="ilosc_komment">komentarzy: '+comments(json2.kids)+'</span></p>';
-                        document.getElementById('booder').innerHTML += wynik;
-                        statisticHowMany(json2.score, 'statystyka_1');
-                        statisticHowMany(comments(json2.kids), 'statystyka_2');
-                        localChanges('statystyka_3', 'img.triangle1');
-                        localChanges('statystyka_4', 'div.hidden');
-                        numberInString(json2.title, 'statystyka_5');
-            })
-        }
-        document.getElementById('statistics').style.display = 'block';
+                for(var a=start; a<howMany; a++) {
+                    let x= json[a];
+                    await fetch('https://hacker-news.firebaseio.com/v0/item/'+x+'.json?print=pretty')
+                        .then(response2 => response2.json())
+                        .then(json2 => {
+                            let wynik = '<div class="post" id="div'+(a+1)+'"><p class="ID_posta">ID: <span id="wynik_ID">'+(a+1)+'</span>';
+                            wynik += '<p class="tytul_posta">';
+                            wynik += '<span id="tytul_posta">tytul: '+json2.title+'</span></p>';
+                            wynik += '<img class="triangle0" id="triangle'+(a+1)+'" src="obrazki/white_triangle.png" onclick="imageChange('+(a+1)+')" style="width: 15px;">&emsp;';
+                            wynik += '<p class="link_posta"><span id="link_posta">';
+                            if(typeof json2.url == 'undefined') {
+                            wynik += '<a href="#">brak</a>';
+                            }
+                            else {
+                                wynik += '(<a href="'+json2.url+'">link</a>)';
+                            }
+                            wynik += '</span></p><br>';
+                            wynik += '<button class="btn btn-secondary" id="hide" onclick="hide('+(a+1)+')">hide</button>&emsp;';
+                            wynik += '<p class="wynik_posta"><span id="wynik_posta">wynik: '+json2.score+'</span></p>';
+                            wynik += '<p class="autor_posta"><span id="autor_posta">autor: '+json2.by+'</span></p>';
+                            wynik += '<p class="data_posta"><span id="data_posta">'+timeAgo(Date.now()/1000-json2.time)+'</span></p>';
+                            wynik += '<p class="ilosc_komment"><span id="ilosc_komment">komentarzy: '+comments(json2.kids)+'</span></p>';
+                            document.getElementById('booder').innerHTML += wynik;
+                            statisticHowMany(json2.score, 'statystyka_1');
+                            statisticHowMany(comments(json2.kids), 'statystyka_2');
+                            localChanges('statystyka_3', 'img.triangle1');
+                            localChanges('statystyka_4', 'div.hidden');
+                            numberInString(json2.title, 'statystyka_5');
+                })
+            }
+            document.getElementById('statistics').style.display = 'block';
+        
     }) 
 }
 
@@ -131,16 +131,30 @@ const showMore = () => { //pokaz więcej
     showPosts(startArticle, endArticle, articleUrl);
 }
 
-const howManyNews = () => { //ile newsow ma sie pokazywac
-    document.getElementById('statistics').style.display = 'none';
-    resetStatistics();
+const howManyNews = async() => { //ile newsow ma sie pokazywac
     amount = document.getElementById('amountNewsy').value;
-    amount = parseInt(amount);
-    number = amount;
-    endArticle = amount;
-    startArticle = 0;
-    document.getElementById("booder").innerText = "";
-    showPosts(startArticle, endArticle, articleUrl);
+    var good=1;
+    await fetch('https://hacker-news.firebaseio.com/v0/newstories.json?')
+        .then(response => response.json())
+        .then (async json => {
+            if(amount<1 || amount>json.length) {
+                alert('Wybierz ilosc newsow od 1 do ' + json.length);
+                good=0;
+                document.getElementById("booder").innerText = "";
+                return;
+            }
+        });
+        if(good==1) {
+            document.getElementById('statistics').style.display = 'none';
+            resetStatistics();
+            amount = parseInt(amount);
+            number = amount;
+            endArticle = amount;
+            startArticle = 0;
+            document.getElementById("booder").innerText = "";
+            console.log('dwa');
+        }
+        showPosts(startArticle, endArticle, articleUrl);
 }
 
 const timeAgo = (unixTime) => { // miliardy cyfra / sekundy // róznnica bierzący_czas - czas_posta
